323bd5897e51f720afa77cf57fd9d4be8f59ffc2
Katherine Soohoo <katherine.soohoo@ibm.com>
2024-04-24 16:10:07 -0700

    initial commit

---
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -0,0 +1,18 @@
+package edu.sjsu;
+
+public class NameChecker {
+    /**
+     * Checks if a given string is a valid name following these rules:
+     * - Between 2 to 40 characters
+     * - Contains only alphabetic characters, non-consecutive hyphens, or a single quote
+     * - Cannot start with a hyphen or single quote
+     * 
+     * @param input Name to check
+     * @return True if input is a valid name, else false
+     */
+    public static boolean check(String input) {
+        // TODO: implement
+
+        return false;
+    }
+}
