b4fbfda8de797e5ec8956a5a973716643be885f5
Mohit Sonwane <101231709+sonwamoh@users.noreply.github.com>
2024-04-25 23:48:07 -0700

    Update NameChecker.java

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -1,11 +1,19 @@
 package edu.sjsu;
 
 public class NameChecker {
+    /**
+     * Checks whether a given string conforms to specific naming criteria.
+     *
+     * @param input The string to be checked for validity.
+     * @return True if the input string meets the specified criteria, otherwise false.
+     */
     public static boolean check(String input) {
         if (input == null || input.length() < 2 || input.length() > 40) {
             return false;
         }
+
         String regex = "^[A-Za-z]([A-Za-z]|(?<!-)-(?=[A-Za-z])|'(?=[A-Za-z]))*[A-Za-z]$";
+
         return input.matches(regex);
     }
 }
